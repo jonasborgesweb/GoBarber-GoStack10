0aef6e78cb4054005b49ad4372c2ee967e4b712b
jonasborgesweb <jonasborgesweb@gmail.com>
2020-02-25 17:51:47 -0300

    Criado o Metodo para gerar o HASH da Senha do Usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,15 @@
 import Sequelize, { Model } from 'sequelize';
 
+// Importando Bcrypt para gerar o Hash da Senha
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
     static init(sequelize) {
         super.init(
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN,
             },
@@ -13,6 +17,15 @@ class User extends Model {
                 sequelize,
             }
         );
+
+        // Configurando o HASH da Senha
+        this.addHook('beforeSave', async user => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+
+        return this;
     }
 }
 
